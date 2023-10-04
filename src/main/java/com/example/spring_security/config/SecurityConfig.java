a34878d4ce4a117deced5c7220035dd05e066cea
V3NOM1977 <rishabhrawat1977@gmail.com>
2023-10-04 23:18:52 +0530

    Database setup with JPA and SecurityFilterChain config

--- src/main/java/com/example/spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/spring_security/config/SecurityConfig.java
@@ -2,22 +2,23 @@ package com.example.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -33,6 +34,21 @@ public class SecurityConfig {
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> {
+                    csrf.disable();
+                })
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .requestMatchers("/error").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
     // @Bean
